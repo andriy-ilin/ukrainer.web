80e7a2a2dfda7eb81bfb858e5f2a93ccd5cce668
andriy-ilin <andriy.ilin90@gmail.com>
2019-03-01 22:27:48 +0200

    feat: initialize firebase, core api service

---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+class ApiService {
+  fb = firebase;
+
+  get = ref =>
+    this.fb
+      .database()
+      .ref(ref)
+      .once("value")
+      .then(res => res.val());
+
+  create = ({ data, model, link, successFn }) => {
+    const id = this.fb
+      .database()
+      .ref(link)
+      .push().key;
+
+    successFn({ id, data });
+
+    return this.fb
+      .database()
+      .ref(`${link}/${id}`)
+      .set(model);
+  };
+
+  createWithFullLink = ({ data, link }) =>
+    this.fb
+      .database()
+      .ref(`${link}`)
+      .set(data);
+}
+
+export default new ApiService();
