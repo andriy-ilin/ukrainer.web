840c643eb5a69f9fb1fedca5f3c0abd882e21430
andriy-ilin <andriy.ilin90@gmail.com>
2019-03-14 12:57:41 +0200

    ci: refactor job deploy to aws

--- ci/install-aws.sh
+++ ci/install-aws.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+# Install aws and add config
 pip install awscli --upgrade --user
 mkdir -p ~/.aws
 echo '[profile eb-cli]' > ~/.aws/config
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
 echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
-aws s3 sync build/ "s3://$S3_BUCKET" --acl public-read --delete
